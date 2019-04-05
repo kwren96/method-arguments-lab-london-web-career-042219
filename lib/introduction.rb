55040aa950deb3f9a8b3baf9463b56b313c258f8
Katherine Wren <kwren@outlook.com>
2019-04-05 16:23:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
 def introduction (firstname)
- puts "Hi, my name is #{firstname}."
+    puts "Hi, my name is #{firstname}."
+end
+
+def introduction_with_language (firstname, language)
+    puts "Hi, my name is #{firstname} and I am learning to program in #{language}"
 end
