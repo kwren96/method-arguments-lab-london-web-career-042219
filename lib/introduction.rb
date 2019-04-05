2fecdb769faca448badfdf2202596f9772ca172a
Katherine Wren <kwren@outlook.com>
2019-04-05 16:23:35 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction (firstname)
+    puts "Hi, my name is #{firstname}."
+end
+
+def introduction_with_language (firstname, language)
+    puts "Hi, my name is #{firstname} and I am learning to program in #{language}."
+end
